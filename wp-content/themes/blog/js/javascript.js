eb93453c8a6e7f2aa91c5adfd10f2671f02120c6
Dennis Brotzky <brotzky@gmail.com>
2015-05-31 13:27:31 -0400

    Added progress indicator

--- wp-content/themes/blog/js/javascript.js
+++ wp-content/themes/blog/js/javascript.js
@@ -26,3 +26,42 @@ $( document ).ready(function() {
   }
 
 });
+
+
+(function(){
+    var $w = $(window);
+  var $circ = $('.animated-circle');
+  var $progCount = $('.progress-count');
+  var $prog2 = $('.progress-indicator-2');
+
+  var wh, h, sHeight;
+
+  function setSizes(){
+    wh = $w.height();
+    h = $(document).height();
+    console.log('doucment height: ' + h);
+    console.log('window height: ' + wh);
+    sHeight = h - wh;
+  }
+
+  setSizes();
+
+  $w.on('scroll', function(){
+    var perc = Math.max(0, Math.min(1, $w.scrollTop()/sHeight));
+    updateProgress(perc);
+  }).on('resize', function(){
+    setSizes();
+    $w.trigger('scroll');
+  });
+
+  function updateProgress(perc){
+    var circle_offset = 126 * perc;
+    $circ.css({
+      "stroke-dashoffset" : 126 - circle_offset
+    });
+    $progCount.html(Math.round(perc * 100) + "%");
+
+    $prog2.css({width : perc*100 + '%'});
+  }
+
+}());
